e69859dce999b4c8343bdc35f8ad907077c35717
miczed <mighty.miczed@gmail.com>
2022-03-30 16:38:56 +0200

    Prevent duplicate unique key on username

--- db/migrate/20220330113741_add_devise_to_users.rb
+++ db/migrate/20220330113741_add_devise_to_users.rb
@@ -4,7 +4,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :username,           null: false, unique: true, default: ""
+      t.string :username,           null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
